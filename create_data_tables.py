1e03d02d723a21e38af01ac1b86b905cc1f76990
Madeleine <48133520+mthompson64@users.noreply.github.com>
2020-11-23 20:56:33 -0800

    Add files via upload
    Create the SQLite tables needed to model the data

---
+++ create_data_tables.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conn = sqlite3.connect('final_project.db')
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS zipcode_table (
+    zip_id INTEGER PRIMARY KEY,
+    zipcode INTEGER NOT NULL UNIQUE,
+    zipcode_url TEXT,
+    lat FLOAT,
+    long FLOAT,
+    current_pop INTEGER,
+    median_income INTEGER
+)""")
+cur.execute("""CREATE TABLE IF NOT EXISTS charging_locations_table (
+    charger_id INTEGER PRIMARY KEY,
+    zip_id INTEGER,
+    name TEXT,
+    address TEXT,
+    city TEXT,
+    FOREIGN KEY (zip_id) REFERENCES zipcode_table (zip_id)
+)""")
+
+conn.commit()
+conn.close()
